8f811723a20c48392d627dc0a90ba5229d3481c7
SchrodingerZhu <i@zhuyi.fan>
2021-01-17 13:01:22 +0800

    fix: leave space for saved regs

--- src/virtual_mips.cpp
+++ src/virtual_mips.cpp
@@ -589,7 +589,7 @@ std::shared_ptr<VirtReg> CmpBranch::def() const {
 
 std::string Function::next_name() {
     std::stringstream ss;
-    ss << name << "_$$branch$$_" << count++;
+    ss << name << "_$$branch_" << count++;
     return ss.str();
 }
 
